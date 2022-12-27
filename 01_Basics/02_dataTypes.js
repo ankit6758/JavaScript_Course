8844e54c542c54e1ff1854c3c975916f55f01259
ankit <kmr.ankit6758@gmail.com>
2022-12-27 21:14:02 +0530

    datatype

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treate all js code as newer version
+
+// alert( 3 +3 ) // we are using nodeJS not browser
+
+// number => 2 to the power 53
+// bigInt
+// string => ""
+// boolearn  => true/false
+// null  => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof null);      // output is object
+console.log(typeof undefined); //output is undefined
